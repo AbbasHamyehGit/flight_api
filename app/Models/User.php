502351c9108571a2b6c6bfbddc4c05f7acd908a0
Your Name <abbashamyehonline2004@gmail.com>
2024-03-03 13:18:43 +0200

    reverting the user to the initial state

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,24 +2,44 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Passenger extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class User extends Authenticatable
 {
-    use HasFactory;
-    
+    use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
+    protected $dates = ['deleted_at'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
-        'first_name',
-        'last_name',
+        'name',
         'email',
         'password',
-        'date_of_birth',
-        'passport_expiry_date',
     ];
-    
-    public function flights()
-    {
-        return $this->belongsToMany(Flight::class);
-    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
