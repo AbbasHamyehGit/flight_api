ff2ec12a33e1fa8be131ec232e8689f5ca4016af
Your Name <abbashamyehonline2004@gmail.com>
2024-03-20 16:45:17 +0200

    successfully returning the tokens for user login and delete it on logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,34 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
 use App\Models\User;
-use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-
+      
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
-            return response()->json($user);
+            if ($user) {
+                $token = Str::random(60); // Generate a random token
+                $user->remember_token = hash('sha256', $token); // Store the token in the remember_token column
+                $user->save(); // Save the user instance to persist the changes
+                return response()->json(['message' => 'Logged in successfully', 'token' => $token,'user '=>$user]);
+            } else {
+                return response()->json(['error' => 'User not found'], 404);
+            }
         }
 
         return response()->json(['error' => 'Unauthorized'], 401);
     }
 
     public function logout(Request $request)
-    {
-        Auth::logout();
-        if (Auth::check()) {
-            // User is authenticated
-            return response()->json(['message' => 'can not logout ']);
+    {   
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = $request->user();
+            $user->remember_token = null; // Clear the remember_token column
+            $user->save(); // Save the user instance to persist the changes
+            Auth::logout();
+            return response()->json(['message' => $user->name.' Logged out successfully']);
         } else {
-            // User is not authenticated
-            return response()->json(['message' => 'Logged out successfully']);
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
-
-        
     }
 }
