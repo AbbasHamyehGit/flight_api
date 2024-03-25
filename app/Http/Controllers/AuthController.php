d099462f8b9fd4c4d86d3aa676f496b728e1ce45
Your Name <abbashamyehonline2004@gmail.com>
2024-03-25 02:04:18 +0200

    automatically generating the tokens instead of generated it randomly and updating the lgout function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,45 +1,57 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
-
 use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
-      
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-            if ($user) {
-                $token = Str::random(60); // Generate a random token
-                $user->remember_token = hash('sha256', $token); // Store the token in the remember_token column
-                $user->save(); // Save the user instance to persist the changes
-                return response()->json(['message' => 'Logged in successfully', 'token' => $token,'user '=>$user]);
-            } else {
-                return response()->json(['error' => 'User not found'], 404);
-            }
+        $data = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response([
+                'msg' => 'incorrect username or password'
+            ], 401);
         }
 
-        return response()->json(['error' => 'Unauthorized'], 401);
+        $token = $user->createToken('apiToken')->plainTextToken;
+
+        $res = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($res, 201);
     }
 
     public function logout(Request $request)
-    {   
-        $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
-            $user = $request->user();
-            $user->remember_token = null; // Clear the remember_token column
-            $user->save(); // Save the user instance to persist the changes
-            Auth::logout();
-            return response()->json(['message' => $user->name.' Logged out successfully']);
-        } else {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-    }
+{
+    // if (auth()->check()) {
+    //     $user = $request->user();
+    //     $user->tokens()->delete();
+
+    //     return response()->json(['message' => $user->name . ' Logged out successfully']);
+    // } else {
+    //     return response()->json(['error' => 'Unauthorized'], 401);
+    // }
+    
+    auth()->user()->tokens()->delete();
+    return [
+        'message' => 'user logged out'
+    ];
+
+
+}
+
 }
