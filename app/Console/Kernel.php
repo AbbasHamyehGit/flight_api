f2c6315855a7f3374c44012dbdf3ea5a23e23527
Your Name <abbashamyehonline2004@gmail.com>
2024-03-28 11:45:16 +0200

    applying scheduling to run the flight reminders once daily in the kernel file

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,9 +2,9 @@
 
 namespace App\Console;
 
-
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\SendFlightReminders; // Import the SendFlightReminders command
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // Schedule the SendFlightReminders command to run every minute
+        $schedule->command(SendFlightReminders::class)->daily();
     }
     
     /**
