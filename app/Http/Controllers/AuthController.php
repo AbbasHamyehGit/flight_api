9fa1bc78956ecae7533d95b4f6dedf243895a38b
Your Name <abbashamyehonline2004@gmail.com>
2024-04-07 16:05:19 +0300

    Destroying all sessions on user login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,39 +5,69 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Support\Str;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\PersonalAccessToken;
 use Illuminate\Auth\AuthenticationException;
 
+
 class AuthController extends Controller
 {
+    // public function login(Request $request)
+    // {
+    //     $request->validate([
+    //         'email' => 'required|string',
+    //         'password' => 'required|string'
+    //     ]);
+
+    //     $user = User::where('email', $request->email)->first();
+
+    //     if (!$user || !Hash::check($request->password, $user->password)) {
+    //         return response()->json(['message' => 'Invalid credentials']);
+    //     }
+
+    //     // Destroy all sessions upon login
+    //     $request->session()->flush();
+
+    //     $token = $user->createToken('apiToken')->plainTextToken;
+
+    //     $res = [
+    //         'user' => $user,
+    //         'token' => $token
+    //     ];
+
+    //     return response($res, 201);
+    // }
+
+
     public function login(Request $request)
     {
         $request->validate([
             'email' => 'required|string',
             'password' => 'required|string'
         ]);
-
+    
         $user = User::where('email', $request->email)->first();
-
+    
         if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json(['message' => 'invalid credentials']);
-          //  throw new AuthenticationException('Invalid credentials');
+            return response()->json(['message' => 'Invalid credentials']);
         }
-
+    
+        // Revoke all existing tokens for the user
+        $user->tokens()->delete();
+    
+        // Create a new token for the user
         $token = $user->createToken('apiToken')->plainTextToken;
-
+    
         $res = [
             'user' => $user,
             'token' => $token
         ];
-
+    
         return response($res, 201);
     }
-
+    
 
     public function logout(Request $request)
     {
@@ -45,22 +75,4 @@ class AuthController extends Controller
     
         return response()->json(['message' => 'Logged out successfully']);
     }
-// public function logout(Request $request)
-// {
-//     // Retrieve the user's tokens from the database
-//     $tokens = PersonalAccessToken::where('tokenable_type', 'App\Models\User')
-//         ->whereIn('token', $request->bearerToken())
-//         ->get();
-
-//     // Delete each token
-//     foreach ($tokens as $token) {
-//         $token->delete();
-//     }
-
-//     // Return a success message
-//     return [
-//         'message' => 'user logged out'
-//     ];
-// }
-
 }
